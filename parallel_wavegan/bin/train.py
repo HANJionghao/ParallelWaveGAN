8d4bcca630593baae361d9ae020855e9ee120ab7
jhan <hanjionghao@126.com>
2025-05-05 17:35:25 -0400

    fix(train.py): add speaker contrastive loss scaling with configurable options to eval step; use float for compatibility with format like "1e6"

--- parallel_wavegan/bin/train.py
+++ parallel_wavegan/bin/train.py
@@ -590,7 +590,26 @@ class Trainer(object):
             spemb_ref = x_ref[2]["spemb"]
             del x_ref, y_ref # TODO(jhan): May remove this line, and delete ds_ref if exists
             spk_loss = self.criterion["speaker_contra"](y_, spemb_ref)
-            gen_loss += self.config["lambda_speaker_contra"] * spk_loss
+            lambda_speaker_contra_conf = self.config["lambda_speaker_contra"]
+            lambda_speaker_contra_type = lambda_speaker_contra_conf.get(
+                "scaling_type", "constant"
+            )
+            if lambda_speaker_contra_type == "constant":
+                lambda_speaker_contra = float(lambda_speaker_contra_conf["base_weight"])
+            elif lambda_speaker_contra_type == "linear":
+                lambda_speaker_contra = float(
+                    lambda_speaker_contra_conf["base_weight"]
+                ) * min(
+                    self.steps,
+                    float(
+                        lambda_speaker_contra_conf["max_scaling_steps"]
+                    ),
+                )
+            else:
+                raise ValueError(
+                    f"Unsupported lambda_speaker_contra_type: {lambda_speaker_contra_type}"
+                )
+            gen_loss += lambda_speaker_contra * spk_loss
             self.total_eval_loss["eval/speaker_contra_loss"] += spk_loss.item()
 
         #######################
